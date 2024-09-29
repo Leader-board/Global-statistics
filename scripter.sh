c2e92eaacf710bff404565b04bbe43fc8f8b0759
Leaderboard <leader-board@outlook.com>
2024-09-29 15:23:38 +0300

    fix scripts

--- scripter.sh
+++ scripter.sh
@@ -4,14 +4,13 @@ cd /data/project/statanalyser/Global-statistics
 sentence="$(<wiki_list.txt)"
 # wiki_name = ()
 for i in $sentence;
-do mysql --defaults-file="$HOME"/replica.my.cnf -h "$i".analytics.db.svc.wikimedia.cloud "$i"_p -e "SELECT user_name, user_registration, user_editcount from user ORDER BY user_editcount desc;" > "${i}".csv;
+do mysql --defaults-file=../replica.my.cnf -h "$i".analytics.db.svc.wikimedia.cloud "$i"_p -e "SELECT user_name, user_registration, user_editcount from user ORDER BY user_editcount desc;" > "${i}".csv;
 done
 rm -r ../rawcsv
 mkdir -p ../rawcsv
 mv *.csv ../rawcsv
 rm -r ../processed_csv
 mkdir -p ../processed_csv
-cd "Global user table generator"
 time python "global_generator.py"
 time python "pushtowiki.py"
 time python "user_data.py"
