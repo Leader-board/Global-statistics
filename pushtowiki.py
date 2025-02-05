06b2c90e044facf2e0be82391aeafca2db4676be
Leaderboard <leader-board@outlook.com>
2025-02-05 16:14:55 +0530

    try again

--- pushtowiki.py
+++ pushtowiki.py
@@ -86,7 +86,7 @@ def add_categories(wiki_name):
     # find the language
     if wiki_name == 'global':
         return '' # no categories
-    cnx = mysql.connector.connect(option_files='$HOME/replica.my.cnf', host='meta.analytics.db.svc.wikimedia.cloud',
+    cnx = mysql.connector.connect(option_files='replica.my.cnf', host='meta.analytics.db.svc.wikimedia.cloud',
                                   database='meta_p')
 
     # handle exceptions
