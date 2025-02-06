b2e056c1bd75a0177ab436688806764ce23300f7
Leaderboard <leader-board@outlook.com>
2025-02-06 16:25:38 +0530

    try fix

--- pushtowiki.py
+++ pushtowiki.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from datetime import date, timezone, datetime
 from dateutil import parser
 from filehash import FileHash
-
+import pymysql.cursors
 import os
 
 from iso639 import Lang
@@ -198,7 +198,7 @@ def get_wiki_statistics(wiki_name):
         return wiki_cache[wiki_name]
 
     try:
-        cnx = mysql.connector.connect(option_files='replica.my.cnf', host=f'{wiki_name}.analytics.db.svc.wikimedia.cloud',
+        cnx = pymysql.connect(option_files='replica.my.cnf', host=f'{wiki_name}.analytics.db.svc.wikimedia.cloud',
                                             database=f'{wiki_name}_p', buffered=True)
         query = "SELECT user_name, user_registration, user_editcount from user WHERE user_editcount > 0 ORDER BY user_editcount desc"
         cursor = cnx.cursor()
