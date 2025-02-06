3c3549965b233b129c28811ef9ece629857d5641
Leaderboard <leader-board@outlook.com>
2025-02-06 16:49:55 +0530

    size cut

--- pushtowiki.py
+++ pushtowiki.py
@@ -200,10 +200,10 @@ def get_wiki_statistics(wiki_name):
     try:
         cnx = pymysql.connect(read_default_file='replica.my.cnf', host=f'{wiki_name}.analytics.db.svc.wikimedia.cloud',
                                             database=f'{wiki_name}_p')
-        query = "SELECT user_name, user_registration, user_editcount from user WHERE user_editcount > 0 ORDER BY user_editcount desc"
-        cursor = cnx.cursor(pymysql.cursors.SSCursor)
+        query = "SELECT user_name, user_registration, user_editcount from user WHERE user_editcount > 0 ORDER BY user_editcount desc LIMIT 1000000"
+        cursor = cnx.cursor()
         cursor.execute(query)
-        res = pd.DataFrame(cursor.fetchall_unbuffered(), columns=[desc[0] for desc in cursor.description])
+        res = pd.DataFrame(cursor.fetchall(), columns=[desc[0] for desc in cursor.description])
         cursor.close()
         return res
     except Exception as e:
