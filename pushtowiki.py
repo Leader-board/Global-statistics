cabc48a2b4bf37d423926f9a37b96c27c44d26ee
Leaderboard <leader-board@outlook.com>
2025-02-07 15:01:21 +0530

    limit workaround

--- pushtowiki.py
+++ pushtowiki.py
@@ -198,14 +198,22 @@ def get_wiki_statistics(wiki_name):
         return wiki_cache[wiki_name]
 
     try:
-        cnx = pymysql.connect(read_default_file='replica.my.cnf', host=f'{wiki_name}.analytics.db.svc.wikimedia.cloud',
-                                            database=f'{wiki_name}_p')
-        query = "SELECT user_name, user_registration, user_editcount from user WHERE user_editcount > 0 ORDER BY user_editcount desc LIMIT 100000"
-        cursor = cnx.cursor()
-        cursor.execute(query)
-        res = pd.DataFrame(cursor.fetchall(), columns=[desc[0] for desc in cursor.description])
-        cursor.close()
-        return res
+        ll = []
+        while True:
+            limit = 200000
+            cnx = pymysql.connect(read_default_file='replica.my.cnf', host=f'{wiki_name}.analytics.db.svc.wikimedia.cloud',
+                                                database=f'{wiki_name}_p')
+            query = """SELECT user_name, user_registration, user_editcount from user WHERE user_editcount > 0 ORDER BY user_editcount desc LIMIT {limit}""".format(limit = limit)
+            cursor = cnx.cursor()
+            cursor.execute(query)
+            res = pd.DataFrame(cursor.fetchall(), columns=[desc[0] for desc in cursor.description])
+            cursor.close()
+            ll.append(res)
+            if len(res) < limit:
+                break
+
+        return pd.concat(ll, ignore_index=True, sort=False)
+
     except Exception as e:
         print(f"Error in {wiki_name}")
         print(e)
