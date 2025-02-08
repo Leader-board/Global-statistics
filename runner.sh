68e61c48506a4d2c658079dc654128cd141f2efe
Leaderboard <leader-board@outlook.com>
2025-02-08 16:08:58 +0530

    reference absolute location

--- runner.sh
+++ runner.sh
@@ -2,6 +2,6 @@
 #cd /Global-statistics
 git pull
 #pip install --user  # useful if not already installed
-mysql --defaults-file=../replica.my.cnf -h meta.analytics.db.svc.wikimedia.cloud meta_p -e "SELECT dbname FROM wiki;"> wiki_list.txt
+/usr/bin/mysql --defaults-file=../replica.my.cnf -h meta.analytics.db.svc.wikimedia.cloud meta_p -e "SELECT dbname FROM wiki;"> wiki_list.txt
 sed -i '1d' wiki_list.txt # delete first line https://stackoverflow.com/questions/339483/how-can-i-remove-the-first-line-of-a-text-file-using-bash-sed-script
 time ./scripter.sh
