4649f2a177fbbb72ff866974689d80073eca3736
Leaderboard <leader-board@outlook.com>
2025-02-08 16:38:04 +0530

    reverse this

--- pushtowiki.py
+++ pushtowiki.py
@@ -198,25 +198,14 @@ def get_wiki_statistics(wiki_name):
         return wiki_cache[wiki_name]
 
     try:
-        ll = []
-        limit = 600000
-        offset = 0
-        while True:
-            print(f"Offset = {offset}")
-            cnx = pymysql.connect(read_default_file='replica.my.cnf', host=f'{wiki_name}.analytics.db.svc.wikimedia.cloud',
-                                                database=f'{wiki_name}_p')
-            query = """SELECT user_name, user_registration, user_editcount from user WHERE user_editcount > 0 ORDER BY user_editcount desc LIMIT {limit} OFFSET {offset}""".format(limit = limit, offset = offset)
-            cursor = cnx.cursor()
-            cursor.execute(query)
-            res = pd.DataFrame(cursor.fetchall(), columns=[desc[0] for desc in cursor.description])
-            ll.append(res)
-            offset += limit
-            cursor.close()
-            if len(res) < limit:
-                break
-
-        return pd.concat(ll, ignore_index=True, sort=False)
-
+        cnx = pymysql.connect(read_default_file='replica.my.cnf', host=f'{wiki_name}.analytics.db.svc.wikimedia.cloud',
+                              database=f'{wiki_name}_p')
+        query = "SELECT user_name, user_registration, user_editcount from user WHERE user_editcount > 0 ORDER BY user_editcount desc"
+        cursor = cnx.cursor(pymysql.cursors.SSCursor)
+        cursor.execute(query)
+        res = pd.DataFrame(cursor.fetchall_unbuffered(), columns=[desc[0] for desc in cursor.description])
+        cursor.close()
+        return res
     except Exception as e:
         print(f"Error in {wiki_name}")
         print(e)
