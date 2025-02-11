4bfbe80562f4c281d6567ded1c506cee19d71ad8
Leaderboard <leader-board@outlook.com>
2025-02-11 10:41:27 +0530

    some fixes

--- pushtowiki.py
+++ pushtowiki.py
@@ -49,6 +49,7 @@ def convert_to_string(fileloc, rankinc, wiki_name=None, existing_df = None):
     # df on its own is useful when graphing
 
     full_df = df.copy(deep=True)
+    df['Registration_date'].fillna('0', inplace=True)
     df.loc[df['Registration_date'].astype(str) == '[None]', 'Registration_date'] = '0'  # remove nan
     df.loc[df['Registration_date'].astype(str) == '', 'Registration_date'] = '0'  # remove nan
     df['Registration_date'] = df['Registration_date'].astype(int)
