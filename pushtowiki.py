4d3719cce5d1f87e1eab05bb80e22c220a9db68c
Leaderboard <leader-board@outlook.com>
2025-02-11 10:47:26 +0530

    exclude temp accounts - probably separate them

--- pushtowiki.py
+++ pushtowiki.py
@@ -202,7 +202,7 @@ def get_wiki_statistics(wiki_name):
     try:
         cnx = pymysql.connect(read_default_file='/root/replica.my.cnf', host=f'{wiki_name}.analytics.db.svc.wikimedia.cloud',
                               database=f'{wiki_name}_p')
-        query = "SELECT user_name, user_registration, user_editcount from user WHERE user_editcount > 0 ORDER BY user_editcount desc"
+        query = "SELECT user_name, user_registration, user_editcount from user WHERE user_editcount > 0 AND user_is_temp = false ORDER BY user_editcount desc"
         cursor = cnx.cursor(pymysql.cursors.SSCursor)
         cursor.execute(query)
         res = pd.DataFrame(cursor.fetchall_unbuffered(), columns=[desc[0] for desc in cursor.description])
@@ -370,7 +370,7 @@ def main():
 
     combined_df = pd.concat(df_list).groupby(['Username', 'Registration_date'])['Edits'].sum().reset_index()
 
-    print(combined_df)
+    push_to_wiki('Rank data/Global', combined_df)
     stp, df, graph_df = convert_to_string('', False , 'global', combined_df)
 
 
