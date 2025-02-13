4d87ba87c17f8db5cedbe6ff982b38ded9d7b2c4
Leaderboard <leader-board@outlook.com>
2025-02-13 11:15:15 +0530

    debug

--- user_data.py
+++ user_data.py
@@ -64,8 +64,12 @@ def analyse_user(username):
         # we want the user's percentile and edit count for each wiki
         usercount, rank, percentile = percentile_and_user_count(username, f)
 
+        print (username, rank, percentile)
+
         if usercount != 0:
             percentile_toprint += convert_to_string(f, usercount, rank, percentile)
+
+        print (percentile_toprint)
     percentile_toprint += '|}\n\n'
     return percentile_toprint
 
