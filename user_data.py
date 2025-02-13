b6ab02cfebd0da719c7cf1778e0b95041642dda0
Leaderboard <leader-board@outlook.com>
2025-02-13 11:37:39 +0530

    fix

--- user_data.py
+++ user_data.py
@@ -45,7 +45,7 @@ def percentile_and_user_count(username, wiki_name):
 
     rank = df_user['Rank'].item()
     usercount = df_user['Edits'].item()
-    percentile = stats.percentileofscore(df_user['Edits'], usercount, kind='strict')
+    percentile = stats.percentileofscore(df['Edits'], usercount, kind='strict')
     return usercount, rank, percentile
 
 
