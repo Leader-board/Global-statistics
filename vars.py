7bc9860974fd36f4404423a598c316a4af1cfcee
Leaderboard <leader-board@outlook.com>
2025-03-11 15:56:16 +0300

    fixes

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 from diskcache import Cache
 
-wiki_cache = Cache(size_limit=int(4e9))
+wiki_cache = Cache(size_limit=int(2e9))
 def get_wiki_set():
     with open('wiki_list.txt', 'r') as f:
         wiki_set = f.read().splitlines()
