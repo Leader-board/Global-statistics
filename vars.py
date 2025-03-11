4d1a00c5cb518f1f0c757cb1165ee1ad65342b64
Leaderboard <leader-board@outlook.com>
2025-03-11 18:00:04 +0300

    try size increase

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 from diskcache import Cache
 
-wiki_cache = Cache()
+wiki_cache = Cache(size_limit=int(8e9))
 def get_wiki_set():
     with open('wiki_list.txt', 'r') as f:
         wiki_set = f.read().splitlines()
