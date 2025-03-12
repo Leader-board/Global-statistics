a1f0e489615ebe733d362c3178d206323e089f1f
Leaderboard <leader-board@outlook.com>
2025-03-12 09:56:53 +0300

    change location to try to improve speed

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 from diskcache import Cache
 
-wiki_cache = Cache(size_limit=2**33, directory='/mnt/nfs/secondary-scratch/global-statistics')
+wiki_cache = Cache(size_limit=int(1.3e10), directory='/tmp/global-statistics')
 def get_wiki_set():
     with open('wiki_list.txt', 'r') as f:
         wiki_set = f.read().splitlines()
